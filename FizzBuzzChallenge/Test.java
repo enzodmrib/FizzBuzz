fbb927f9f4c4d149055d63ae4937e822a5e321cb
enzodmrib <enzorib2@gmail.com>
2020-05-19 23:37:58 -0300

    FizzBuzz Challenge

--- FizzBuzzChallenge/Test.java
+++ FizzBuzzChallenge/Test.java
@@ -1,6 +1,9 @@
 public class Test {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        int[] arr = new int[1000];
+        for(int i=0; i<1000 ; i++){
+            arr[i] = i+1;
+        }
         FizzBuzz.function(arr);
     }
 }
