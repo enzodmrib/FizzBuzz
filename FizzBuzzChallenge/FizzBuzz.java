7d43ce2b3be3ff5ada4c3988b179e2b94b568e3e
enzodmrib <enzorib2@gmail.com>
2020-05-19 21:57:46 -0300

    FizzBuzz Challenge

--- FizzBuzzChallenge/FizzBuzz.java
+++ FizzBuzzChallenge/FizzBuzz.java
@@ -1,15 +1,14 @@
-
 public class FizzBuzz{
-    public static void function(int[] n){
+    public static void function(int[] n){               //static para utilizar a função sem instanciar
         int fizzBuzzCounter, fizzCounter, buzzCounter;
         fizzBuzzCounter = fizzCounter = buzzCounter = 0;
         
-        for(int i=0;i<n.length;i++){
-            if (n[i]%3==0 && n[i]%5==0){
-                fizzBuzzCounter++;
+        for(int i=0;i<n.length;i++){                    //loop até o tamanho do conjunto n
+            if (n[i]%3==0 && n[i]%5==0){                //comparação para checar multiplos
+                fizzBuzzCounter++;                      //soma no contador do caso específico
                 System.out.println("FizzBuzz");
             } 
-            else if (n[i]%3==0){
+            else if (n[i]%3==0){                        
                 System.out.println("Fizz");
                 fizzCounter++;
             } 
@@ -17,7 +16,7 @@ public class FizzBuzz{
                 System.out.println("Buzz");
                 buzzCounter++;
             } 
-            else{
+            else{                                       //caso não seja múltiplo, retorna o elemento
                 System.out.println(n[i]);
             }
         }
